27155e0e4546eb8420374654b4a4b06da7742dc8
rizkimuliono <rizkimuliono@gmail.com>
2024-05-30 13:44:47 +0700

    update file layouts

--- resources/views/layouts.blade.php
+++ resources/views/layouts.blade.php
@@ -111,8 +111,6 @@
                     <span>LOG OUT</span></a>
             </li>
 
-
-
             <!-- Sidebar Toggler (Sidebar) -->
             <div class="text-center d-none d-md-inline">
                 <button class="rounded-circle border-0" id="sidebarToggle"></button>
